2cb2ed64d78940e72abafe3459b04f403d271eed
inwick <indikasandun2015@gmail.com>
2022-04-30 22:17:23 +0530

    Added navebar changes and supervisor home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
@@ -11,22 +11,24 @@ import AdminDocUpload from './components/admin-doc-upload.component';
 import AdminHome from './components/admin-home.component';
 import EditAdminFile from './components/admin-doc-edit.component';
 import AdminFileList from './components/uploaded-files-admin.component';
+import SupervisorHome from './components/supervisor-components/supervisor-home.component';
 
 function App() {
   return (
     <Router>
       <div className="container">
-      <Navbar />
-      <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
-      <Route path="/admin-file-upload" component={AdminDocUpload} />
-      <Route path="/admin-file-download" component={AdminFileList} />
-      <Route path="/admin-file-edit/:id" component={EditAdminFile} />
-      <Route path="/admin-home" component={AdminHome} />
-      
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+        <Route path="/admin-file-upload" component={AdminDocUpload} />
+        <Route path="/admin-file-download" component={AdminFileList} />
+        <Route path="/admin-file-edit/:id" component={EditAdminFile} />
+        <Route path="/admin-home" component={AdminHome} />
+        <Route path="/supervisor-home" component={SupervisorHome} />
+
       </div>
     </Router>
   );
