3f43643bfbb2bbb68990a10142375ce02a22ec65
dullbull97 <it20205638@my.sliit.lk>
2022-06-05 00:22:27 +0530

    Unit tests initialized

--- tests/.adminAPITests.spec.js
+++ tests/.adminAPITests.spec.js
@@ -4,18 +4,18 @@ import "regenerator-runtime/runtime";
 describe("Sample test suit", () => {
   it("tests / API TEST/ GET specific student", async () => {
     await axios
-      .get(`http://localhost:5000/admin/getFile/629b768e67f6181abbf1bbab`)
+      .get(`http://localhost:5000/admin/getFile/629b9f3f071d269118a83a55`)
       .then((res) => {
         expect(res.data).toEqual({
-          _id: "629b768e67f6181abbf1bbab",
-          title: "Sample File",
-          description: "Sample File",
-          file_path: "AdminFiles\\1654355598284_Code Coverage Lab.pdf",
-          file_mimetype: "application/pdf",
-          createdAt: "2022-06-04T15:13:18.295Z",
-          updatedAt: "2022-06-04T15:13:18.295Z",
-          __v: 0,
-        });
+          "_id": "629b9f3f071d269118a83a55",
+          "title": "Sample File 02",
+          "description": "Sample file 02",
+          "file_path": "AdminFiles\\1654366015663_Functional and Non-functional Testing.pdf",
+          "file_mimetype": "application/pdf",
+          "createdAt": "2022-06-04T18:06:55.747Z",
+          "updatedAt": "2022-06-04T18:08:05.956Z",
+          "__v": 0
+      });
         expect(res.status).toBe(200);
       });
   });
